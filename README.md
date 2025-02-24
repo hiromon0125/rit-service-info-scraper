6d781924fb67e52fa282f6fece06c4d26a25c2ab
Hiro <hiroto125takeuchi@gmail.com>
2025-02-24 18:32:26 -0500

    feat: some more content

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
+This is a project for rit bus schedule where it needs to scrape the service info on rit bus website and serve data for the main site. 
+
+The technologies used are cloudflare workers, hono, axios, and cheerio.
+
+
+## Setup
 ```
 pnpm install
 pnpm run dev
 ```
-
+for environmental variables:
 ```
+cp example.vars .dev.vars
+```
+Change the variables to your setup.
+> [!NOTE]
+> Vars is cloudflare's way of doing environmetal variables. This is where sercret keys are stored for cloudflare workers. I would also recommend creating the same file for production environment with `cp example.vars .prod.vars`.
+
+This app has a simple secret key protection that needs to be set on the header to work. While this isn't the most secure way to do it, its the simplest and does the job for most part. The rest will be secured by strictly requesting it through server side and never reveal on the client code.
+
+
+## Deployment
+```
+pnpm wrangler login
 pnpm run deploy
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,81 @@
-import { Hono } from 'hono'
+import * as cheerio from 'cheerio';
+import { Hono } from 'hono';
 
-const app = new Hono()
+type Bindings = {
+	TARGET_URL: string;
+	SECRET_KEY: string;
+	NODE_ENV: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.use('*', async (c, next) => {
+	if (!c.env.SECRET_KEY) {
+		return c.text('SECRET_KEY is not set', 500);
+	}
+	if (
+		c.env.NODE_ENV != 'development' &&
+		c.env.SECRET_KEY !== c.req.header('X-Secret-Key')
+	) {
+		return c.text('Unauthorized', 401);
+	}
+	console.log('Request received', {
+		ENV: c.env.NODE_ENV,
+	});
+
+	await next();
+});
+app.get('/', async (c) => {
+	const targetUrl = c.env.TARGET_URL;
+	if (!targetUrl) {
+		return c.text('TARGET_URL is not set', 500);
+	}
+
+	try {
+		const response = await fetch(targetUrl);
+		if (!response.ok) throw new Error('Failed to fetch the page');
+
+		const html = await response.text();
+		const $ = cheerio.load(html);
+
+		// Extract some example data (e.g., all headings)
+		const text = $('p.h1')
+			.parent()
+			.map((_, el) =>
+				$(el)
+					.text()
+					.split('\n')
+					.filter((text) => text.trim() !== '')
+					.slice(1)
+					.reduce((acc, text, index) => {
+						if (index % 2 !== 1) {
+							const sanitizedText = text.trim().split(':');
+							const title = sanitizedText[0];
+							const buses = sanitizedText[1]
+								.split(',')
+								.map((bus) => bus.trim());
+							const newAcc = [...acc, { title, buses }];
+							return newAcc;
+						} else {
+							acc[acc.length - 1].content = text.trim();
+							return acc;
+						}
+					}, [] as { title?: string; content?: string }[])
+			)
+			.get();
+
+		return c.json({ targetUrl, text });
+	} catch (error) {
+		return c.json(
+			{
+				error,
+				...(c.env.NODE_ENV === 'development' && error instanceof Error
+					? { message: error.message }
+					: {}),
+			},
+			500
+		);
+	}
+});
+
+export default app;

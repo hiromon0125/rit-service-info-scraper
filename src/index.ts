659b12e6a2cb89a9f81f790ab787fb2f44e0fb50
Hiro <hiroto125takeuchi@gmail.com>
2025-02-24 21:59:30 -0500

    feat: more detailed cacheing and data

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import * as cheerio from 'cheerio';
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { md5 } from 'hono/utils/crypto';
 
 type Bindings = {
 	TARGET_URL: string;
 	SECRET_KEY: string;
 	NODE_ENV: string;
+	KV: KVNamespace;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use('*', async (c, next) => {
+app.use(logger()).use('*', async (c, next) => {
 	if (!c.env.SECRET_KEY) {
 		return c.text('SECRET_KEY is not set', 500);
 	}
@@ -19,9 +22,6 @@ app.use('*', async (c, next) => {
 	) {
 		return c.text('Unauthorized', 401);
 	}
-	console.log('Request received', {
-		ENV: c.env.NODE_ENV,
-	});
 
 	await next();
 });
@@ -31,51 +31,112 @@ app.get('/', async (c) => {
 		return c.text('TARGET_URL is not set', 500);
 	}
 
-	try {
-		const response = await fetch(targetUrl);
-		if (!response.ok) throw new Error('Failed to fetch the page');
+	const response = await fetch(targetUrl);
+	if (!response.ok) return c.json({ error: 'Failed to fetch data' }, 500);
 
-		const html = await response.text();
-		const $ = cheerio.load(html);
+	const html = await response.text();
+	const $ = cheerio.load(html);
 
-		// Extract some example data (e.g., all headings)
-		const text = $('p.h1')
-			.parent()
-			.map((_, el) =>
-				$(el)
-					.text()
-					.split('\n')
-					.filter((text) => text.trim() !== '')
-					.slice(1)
-					.reduce((acc, text, index) => {
-						if (index % 2 !== 1) {
-							const sanitizedText = text.trim().split(':');
-							const title = sanitizedText[0];
-							const buses = sanitizedText[1]
-								.split(',')
-								.map((bus) => bus.trim());
-							const newAcc = [...acc, { title, buses }];
-							return newAcc;
-						} else {
-							acc[acc.length - 1].content = text.trim();
-							return acc;
-						}
-					}, [] as { title?: string; content?: string }[])
-			)
-			.get();
+	// Extract some example data (e.g., all headings)
+	const text = $('p.h1')
+		.parent()
+		.map((_, el) => $(el).text().split('\n'))
+		.get()
+		.filter((text) => text.trim() !== '')
+		.slice(1)
+		.reduce((acc, text, index) => {
+			if (index % 2 !== 1) {
+				const sanitizedText = text.trim().split(':');
+				const title = sanitizedText[0];
+				const buses = sanitizedText[1]
+					.split(',')
+					.map((bus) => bus.trim());
+				const newAcc = [...acc, { title, buses }];
+				return newAcc;
+			} else {
+				acc[acc.length - 1].content = text.trim();
+				return acc;
+			}
+		}, [] as { title?: string; content?: string; buses: string[] }[]);
+	if (text.filter((item) => item.title || item.content).length === 0) {
+		return c.json({ error: 'No valid data found' }, 500);
+	}
+	// Generate hashes for each object stringified
+	const hashes = await Promise.allSettled(
+		text.map(async (item) => {
+			const jsonText = JSON.stringify(item);
+			return { hash: await md5(jsonText), data: item };
+		})
+	);
+	if (
+		!hashes.every((hash) => hash.status != 'rejected') ||
+		hashes.map((hash) => hash.value.hash).includes(null)
+	) {
+		return c.json({ error: 'Failed to generate hashes' }, 500);
+	}
 
-		return c.json({ targetUrl, text });
-	} catch (error) {
+	// Check if the hashes are already in the cache
+	// If not, store the new values in the cache with the current timestamp
+	// If yes, return the cached values
+	const finalValues = await Promise.allSettled(
+		(
+			hashes as PromiseFulfilledResult<{
+				hash: string;
+				data: { title: string; content: string; buses: string[] };
+			}>[]
+		).map(async (hash) => {
+			const cached = await c.env.KV.get<{
+				title?: string;
+				content?: string;
+				buses: string[];
+				hash: string;
+				timestamp: number;
+			}>(hash.value.hash, 'json');
+
+			if (cached) return { ...cached, isNew: false };
+			const newValue = {
+				...hash.value.data,
+				hash: hash.value.hash,
+				timestamp: Date.now(),
+			};
+			await c.env.KV.put(hash.value.hash, JSON.stringify(newValue));
+			return {
+				...newValue,
+				isNew: true,
+			};
+		})
+	);
+	console.log('Final values:', finalValues);
+
+	let errorList;
+	if (
+		(errorList = finalValues.filter((value) => value.status == 'rejected'))
+			.length > 0
+	) {
 		return c.json(
 			{
-				error,
-				...(c.env.NODE_ENV === 'development' && error instanceof Error
-					? { message: error.message }
+				error: 'Failed to generate final values',
+				...(c.env.NODE_ENV == 'development'
+					? { error_objects: errorList }
 					: {}),
 			},
 			500
 		);
 	}
+	const finalValuesResolved = finalValues as PromiseFulfilledResult<{
+		title: string;
+		content: string;
+		buses: string[];
+		hash: string;
+		timestamp: number;
+		isNew: boolean;
+	}>[];
+	console.log('Final values:', finalValuesResolved);
+
+	return c.json({
+		targetUrl,
+		data: finalValuesResolved.map((value) => value.value),
+	});
 });
 
 export default app;
